73c61b2f7c3f3c713aea6994298c7c70320d7127
GEDDE5 <geddes.3754@gmail.com>
2017-02-14 22:14:30 +0000

    Adds logic to /urls route to handle urlDatabase writing

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
-const server = require("express")();
+const server = require('express')();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
+const randomStr = require('./make-shorturl')
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.set("view engine", "ejs");
@@ -14,10 +15,10 @@ server.get('/urls', (request, response) => {
   response.render('urls_index', { urls: urlDatabase })
 });
 
-server.get('/urls/:id', (request, response) => {
-  let templateVars = { shortURL: request.params.id, urls: urlDatabase };
-  response.render('urls_show', templateVars);
-});
+// server.get('/urls/:id', (request, response) => {
+//   let templateVars = { shortURL: request.params.id, urls: urlDatabase };
+//   response.render('urls_show', templateVars);
+// });
 
 server.get('/urls/new', (request, response) => {
   response.render('urls_new');
@@ -25,6 +26,14 @@ server.get('/urls/new', (request, response) => {
 
 server.post('/urls', (request, response) => {
   console.log(request.body);
+  let str = randomStr.get();
+  if (urlDatabase[str]) {
+    while (urlDatabase[str]) {
+      str = randomStr.get();
+    }
+  }
+  urlDatabase[str] = request.body['longURL'];
+  console.log(urlDatabase);
   response.send('OK');
 })
 
