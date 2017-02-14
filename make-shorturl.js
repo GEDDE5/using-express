e4e8704a11a2f93a32cbd2f2da08701af26a93f7
GEDDE5 <geddes.3754@gmail.com>
2017-02-14 21:13:32 +0000

    Adds very minor modularity

--- make-shorturl.js
+++ make-shorturl.js
@@ -2,7 +2,7 @@ function generateRandomString() {
   let chars = 'abcdefghijklmnopqrstufwxyzABCDEFGHIJKLMNOPQRSTUFWXYZ1234567890';
   let str = '';
   for (let x = 0; x < 6; x++) {
-    str += chars[Math.ceil(Math.random() * 62)];
+    str += chars[Math.ceil(Math.random() * chars.length)];
   }
   return console.log(str);
 }
