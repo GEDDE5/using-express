92aab946f4010bf2021231e595fdb91d2c33ae7b
GEDDE5 <geddes.3754@gmail.com>
2017-02-14 20:11:00 +0000

    First commit (a bit late...)

---
+++ express_server.js
@@ -0,0 +1,35 @@
+const server = require("express")();
+const PORT = process.env.PORT || 8080;
+
+server.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+server.get('/urls', (request, response) => {
+  response.render('urls_index', { urls: urlDatabase })
+});
+
+server.get('/urls/:id', (request, response) => {
+  let templateVars = { shortURL: request.params.id, urls: urlDatabase };
+  response.render('urls_show', templateVars);
+});
+
+// server.get("/", (request, response) => {
+//   console.log(request);
+//   response.send("Hello!");
+// });
+
+// server.get("/urls.json", (request, response) => {
+//   response.json(urlDatabase);
+// });
+
+// server.get('/hello', (request, response) => {
+//   response.send('<html><body>Hello<b>World</b></body></html>\n');
+// })
+
+server.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
