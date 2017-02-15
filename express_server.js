7ce31596cfe4a6f065294dfa689add538800c4da
GEDDE5 <geddes.3754@gmail.com>
2017-02-15 16:53:11 +0000

    Adds functionality to delete short URL entries

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
-const server = require('express')();
+const express = require('express');
+server = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
-const randomStr = require('./make-shorturl')
+const randomStr = require('./make-shorturl');
 
+server.use(express.static('public'));
 server.use(bodyParser.urlencoded({ extended: true }));
 server.set("view engine", "ejs");
 
@@ -12,21 +14,29 @@ const urlDatabase = {
 };
 
 server.get('/urls', (request, response) => {
-  response.render('urls_index', { urls: urlDatabase })
+  response.render('urls_index', { urls: urlDatabase });
 });
 
 server.post('/urls', (request, response) => {
-
-  // only adds shortURL to database if it doesn't already exist
+  // if shortURL alreadys exists in database, generate one
+  // until shortURL's value cannot be found in urlDatabase
   let str = randomStr.get();
   if (urlDatabase[str]) {
     while (urlDatabase[str]) {
       str = randomStr.get();
     }
   }
-  urlDatabase[str] = request.body['longURL'];
+  // if submitted URL does not include http://, prepend
+  // it to longURL and save it to urlDatabase
+  let longURL = request.body['longURL'];
+  let scheme='http://';
+  if (!request.body['longURL'].includes('http://')) {
+    longURL = scheme + longURL;
+  }
+  // Adds a new key-value pair, ie. shortURL: longURL
+  urlDatabase[str] = longURL;
   response.redirect('/urls/' + str);
-})
+});
 
 server.get('/urls/new', (request, response) => {
   response.render('urls_new');
@@ -37,28 +47,19 @@ server.get('/urls/:id', (request, response) => {
   response.render('urls_show', templateVars);
 });
 
+server.post('/urls/:id/delete', (request, response) => {
+  delete urlDatabase[request.params.id];
+  response.redirect('/urls');
+});
+
 server.get('/u/:shortURL', (request, response) => {
-  let shortURL = request.params.shortURL
-  let longURL = urlDatabase[shortURL];
-  let protocol = 'http://';
-  if (!longURL.includes('http://')) {
-    longURL = protocol + longURL;
+  let shortURL = request.params.shortURL;
+  if(!urlDatabase[shortURL]) {
+    response.redirect('/urls');
   }
+  let longURL = urlDatabase[shortURL];
   response.redirect(longURL);
-})
-
-// server.get("/", (request, response) => {
-//   console.log(request);
-//   response.send("Hello!");
-// });
-
-// server.get("/urls.json", (request, response) => {
-//   response.json(urlDatabase);
-// });
-
-// server.get('/hello', (request, response) => {
-//   response.send('<html><body>Hello<b>World</b></body></html>\n');
-// })
+});
 
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
